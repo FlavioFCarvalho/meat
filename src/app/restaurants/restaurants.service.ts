889fb8555f72469dd619074255adc9df5168b1b9
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-02-20 10:03:38 -0300

    commit bruto

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -18,9 +18,16 @@ export class RestaurantsService {
      
     constructor(private http: Http){}
     
+    //lista restaurantes
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
         .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
+    //lista um restaurante
+   restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handleError)
+      }
 }
