58d523870419359ade230fba3ea58c1713d067d5
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-02-20 15:17:12 -0300

    Serviço HTTP para o Detalhe do Restaurante

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant
+
+  constructor(private RestaurantsService: RestaurantsService,
+     private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.RestaurantsService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant)
   }
 
 }
