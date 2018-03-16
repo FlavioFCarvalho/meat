a214bcbd2f125e40cefec1da65259692fc5a688d
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-03-16 10:04:32 -0300

    Criando um Feature Module (Módulo de Compra)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,10 +19,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
 import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -42,9 +39,6 @@ import { SharedModule } from './shared/shared.module';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSumaryComponent
   ],
   imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 
 export const ROUTES: Routes = [
@@ -20,5 +19,7 @@ export const ROUTES: Routes = [
         ]},
     {path: 'about', loadChildren:'./about/about.module#AboutModule'},
     {path: 'order-sumary', component: OrderSumaryComponent},
-    {path: 'order', component: OrderComponent}
+    {path: 'order', loadChildren: './order/order.module#OrderModule'}
+   
+    
 ]
