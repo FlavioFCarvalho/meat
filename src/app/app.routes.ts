f2d8d573a644725a6f62160974dd436eb9532d3e
Flávio Carvalho <flaviocfr@gmail.com>
2019-11-06 15:25:00 -0300

    Preparando a Página de Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,31 @@
-import {Routes} from '@angular/router';
+import { Routes } from "@angular/router";
 
-import { HomeComponent } from './home/home.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from "./home/home.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
+import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { OrderSumaryComponent } from "./order-sumary/order-sumary.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { LoginComponent } from "./security/login/login.component";
 
 export const ROUTES: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "restaurants", component: RestaurantsComponent },
+  {
+    path: "restaurants/:id",
+    component: RestaurantDetailComponent,
+    children: [
+      { path: "", redirectTo: "menu", pathMatch: "full" },
+      { path: "menu", component: MenuComponent },
+      { path: "reviews", component: ReviewsComponent }
+    ]
+  },
+  { path: "about", loadChildren: "./about/about.module#AboutModule" },
+  { path: "order-sumary", component: OrderSumaryComponent },
+  { path: "order", loadChildren: "./order/order.module#OrderModule" },
+  { path: "**", component: NotFoundComponent }
 
-    {path: '' , component: HomeComponent},
-    {path: 'restaurants', component: RestaurantsComponent},
-    {path: 'restaurants/:id', component: RestaurantDetailComponent,
-        children: [
-            {path: '', redirectTo: 'menu', pathMatch: 'full'},
-            {path: 'menu', component: MenuComponent },
-            {path: 'reviews', component: ReviewsComponent}
-        ]},
-    {path: 'about', loadChildren:'./about/about.module#AboutModule'},
-    {path: 'order-sumary', component: OrderSumaryComponent},
-    {path: 'order', loadChildren: './order/order.module#OrderModule'},
-    {path: '**', component: NotFoundComponent}
-   
-    
-// tslint:disable-next-line:eofline
+  // tslint:disable-next-line:eofline
 ];
