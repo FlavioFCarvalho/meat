a424a21c879fa9e8cbc45ce7b1c6c0d776a340bb
Flávio Carvalho <flaviocfr@gmail.com>
2019-11-06 15:56:28 -0300

    Criando o Componente de Login.

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,39 +1,45 @@
-import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ContentChild,
+  AfterContentInit
+} from "@angular/core";
 
-import {NgModel, FormControlName} from '@angular/forms'; 
+import { NgModel, FormControlName } from "@angular/forms";
 
 @Component({
-  selector: 'mt-input-container',
-  templateUrl: './input.component.html'
+  selector: "mt-input-container",
+  templateUrl: "./input.component.html"
 })
 export class InputComponent implements OnInit, AfterContentInit {
+  @Input() label: string;
+  @Input() errorMessage: string;
+  @Input() showTip: boolean = false;
 
-  @Input() label: string
-  @Input() errorMessage: string 
+  input: any;
 
-  input: any
+  @ContentChild(NgModel) model: NgModel;
+  @ContentChild(FormControlName) control: FormControlName;
 
-  @ContentChild(NgModel) model: NgModel
-  @ContentChild(FormControlName) control: FormControlName
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  ngAfterContentInit(){
-    this.input = this.model || this.control
-    if(this.input === undefined){
-      throw new Error('Esse componente precisa ser usado com uma diretiva ngModel ou formControlName')
+  ngAfterContentInit() {
+    this.input = this.model || this.control;
+    if (this.input === undefined) {
+      throw new Error(
+        "Esse componente precisa ser usado com uma diretiva ngModel ou formControlName"
+      );
     }
   }
 
-  hasSuccess(): boolean{
-    return this.input.valid && (this.input.dirty || this.input.touched)
+  hasSuccess(): boolean {
+    return this.input.valid && (this.input.dirty || this.input.touched);
   }
 
-  hasError(): boolean{
-    return this.input.invalid && (this.input.dirty || this.input.touched)
+  hasError(): boolean {
+    return this.input.invalid && (this.input.dirty || this.input.touched);
   }
-
 }
