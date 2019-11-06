ecc1ed0aeed10d31f21f8f19676bab688d8c35a8
Flávio Carvalho <flaviocfr@gmail.com>
2019-11-06 11:16:36 -0300

    Refactoring da Aplicação.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule, PreloadAllModules } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, PreloadAllModules } from "@angular/router";
 
+import { ROUTES } from "./app.routes";
 
-
-
-import {ROUTES} from './app.routes';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
-import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
-import { SharedModule } from './shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotFoundComponent } from './not-found/not-found.component';
-
-
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
+import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
+import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
+import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { RestaurantDetailComponent } from "app/restaurant-detail/restaurant-detail.component";
+import { OrderSumaryComponent } from "./order-sumary/order-sumary.component";
+import { SharedModule } from "./shared/shared.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 @NgModule({
   declarations: [
@@ -45,11 +40,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpModule,
+    HttpClientModule,
     SharedModule.forRoot(),
-     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{ provide: LOCALE_ID, useValue: "pt-BR" }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,52 +1,49 @@
-import { Injectable } from '@angular/core';
-import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
-import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
-import { Observable } from 'rxjs/Observable';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
+import { Observable } from "rxjs/Observable";
+import { HttpClient } from "@angular/common/http";
+import "rxjs/add/operator/map";
 
-import {Order, OrderItem} from './order.model';
-import { MEAT_API } from '../app.api';
-
-
+import { Order, OrderItem } from "./order.model";
+import { MEAT_API } from "../app.api";
 
 @Injectable()
 export class OrderService {
+  constructor(
+    private cartService: ShoppingCartService,
+    private http: HttpClient
+  ) {}
 
-    constructor (private cartService: ShoppingCartService, private http: Http){}
+  itemsValue(): number {
+    return this.cartService.total();
+  }
 
-    itemsValue(): number{
-        return this.cartService.total()
-    }
+  cartItems(): CartItem[] {
+    return this.cartService.items;
+  }
 
-    cartItems(): CartItem[] {
-        return this.cartService.items 
-    }
+  increaseQty(item: CartItem) {
+    this.cartService.increaseQty(item);
+  }
 
-    increaseQty(item: CartItem){
-        this.cartService.increaseQty(item)
-    }
+  decreaseQty(item: CartItem) {
+    this.cartService.decreaseQty(item);
+  }
 
-    decreaseQty(item: CartItem){
-        this.cartService.decreaseQty(item)
-    }
+  remove(item: CartItem) {
+    this.cartService.removeItem(item);
+  }
 
-    remove(item: CartItem){
-        this.cartService.removeItem(item)
-    }
+  clear() {
+    this.cartService.clear();
+  }
 
-    clear(){
-        this.cartService.clear()
-    }
-
-    checkOrder(order: Order): Observable<string>{
-        const headers = new Headers()
-        headers.append('Content-Type', 'application/json')
-        return this.http.post(`${MEAT_API}/orders`,
-          JSON.stringify(order),
-        new RequestOptions({headers: headers}))
-            .map(response=> response.json())
-            .map(order => order.id)
-
-    }
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    return this.http
+      .post<Order>(`${MEAT_API}/orders`, order)
+      .map(order => order.id);
+  }
 }
